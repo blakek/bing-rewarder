fd58aabbebb44beb40f151c18a6f8a76af94f16a
Blake Knight <oss.ideas@gmail.com>
2016-01-04 21:11:30 -0600

    get search from file functionality working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var request = require('request')
+  , fs = require('fs')
   , exec = require('child_process').exec
   , commander = require('commander')
   , googleNewsApi = require('./lib/google-news-titles')
@@ -8,6 +9,7 @@ var request = require('request')
 commander
   .version('0.0.1')
   .option('-d, --dry-run', 'output what would have been searched, but don\'t actually search')
+  .option('-f, --file [file]', 'file to get random searches from', `${__dirname}/search-list.txt`)
   .option('-g, --google [topic]', 'use Google News API to generate 10 things to search for')
   .option('-n, --num [value]', 'number of queries to perform (not compatible with --google option)', '20')
   .option('-w, --wait [value]', 'milliseconds to wait between requests')
@@ -21,11 +23,33 @@ function prepareSearchString(string) {
     .replace(/ /g, '+')
 }
 
-  return array[Math.floor(Math.random() * array.length)]
 /* Get "length" number of elements from an array without picking the same
  * element twice
  */
 function getRandomFromArray(array, length) {
+  if (typeof length === 'undefined' || length > array.length) {
+    length = array.length
+  }
+
+  for (var i = array.length - 1, c = length; i > 0 && c > 0; i--, c--) {
+    var j = Math.floor(Math.random() * (i + 1))
+        var temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+  }
+
+  return array.slice(array.length - length)
+}
+
+/* Returns random lines in a file as an array
+ */
+function getRandomFromFile(file) {
+  var contents = fs
+    .readFileSync(commander.file, {encoding: 'UTF8'})
+    .trim()
+    .split('\n')
+
+  return getRandomFromArray(contents, commander.num)
 }
 
 /* Get stuff to search for depending on the methods specified
@@ -39,6 +63,8 @@ function getSearchArray(callback) {
         callback(results)
       }
     })
+  } else {
+    callback(getRandomFromFile())
   }
 }
 
